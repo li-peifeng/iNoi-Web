6a0a4de2f414b0930cab6b1c82cc55c4fbd9df0e
“li-peifeng” <peifeng@me.com>
2025-10-23 17:14:47 +0200

    Fix-Password Error

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -1,6 +1,7 @@
 import sha256 from "sha256"
 
-const hash_salt = "https://github.com/li-peifeng/inoi"
+// TODO: use argon2
+const hash_salt = "https://github.com/alist-org/alist"
 
 export function hashPwd(pwd: string) {
   return sha256(`${pwd}-${hash_salt}`)
